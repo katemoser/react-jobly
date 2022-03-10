cf6a16e5a934e02b9ba703bd20fea7974bf7cfbf
edwinkim97 <edwinkim124@gmail.com>
2022-03-09 17:12:48 -0800

    working on routes test with mockAxios

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import {axios, axiosMock} from "axios";
 import { MemoryRouter } from "react-router-dom";
 import Routes from "./Routes";
 
@@ -11,4 +12,28 @@ it("renders the homepage", function(){
     expect(h1Text).toBeInTheDocument();
 })
 
-it("renders Company Detail")
+it("renders Company Detail", async function(){
+    axiosMock.get.mockResolvedValueOnce({ 
+        data: {  
+            handle:"test", 
+            name: "testName", 
+            description: "testDesc", 
+            numEmployees: 5, 
+            logoUrl: "testUrl",
+            jobs: [{ id:"testId", 
+                     title:"testTitle", 
+                     salary: 10000, 
+                     equity: .1 
+                    }] 
+                } 
+    });
+
+    const {debug, getByText} = render(
+        <MemoryRouter initialEntries={["/companies/test"]}>
+            <Routes />
+        </MemoryRouter>
+    );
+
+    const companyName = getByText("testName");
+    expect(companyName).toBeInTheDocument();
+})
